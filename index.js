353298e72f2599958a77e26d202b429075cfba4d
JaswanthRemiel <jaswanthremiel7@gmail.com>
2025-04-15 23:25:11 +0530

    CORS

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ import roasthubRoute from './routes/roasthub.js';
 dotenv.config();
 const app = express();
 
-const allowedOrigins = ['/^https:\/\/.*\.remiel\.fyi$/']; 
+const allowedOriginRegex = /^https:\/\/remiel\.fyi$/;
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin || allowedOriginRegex.test(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
 }));
-app.use(express.json());
 
+app.use(express.json());
 app.use('/roasthub', roasthubRoute);
 
 const PORT = process.env.PORT || 3000;
