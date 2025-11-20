fa14508e27efc81a609908dcaab3d0955be5a388
JaswanthRemiel <jaswanthremiel7@gmail.com>
2025-11-20 21:12:57 +0530

    updated

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ import router from "./router.js";
 dotenv.config();
 const app = express();
 
-const allowedOriginRegex = /^https:\/\/remiel\.work$/;
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOriginRegex.test(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
-
 app.use(express.json());
 app.use("/", router);
 
